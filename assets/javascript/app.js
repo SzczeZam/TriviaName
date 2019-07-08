4d1d3136c1db27b48a507211c05c906a39a29c18
Max Szczepaniak <wookslandline@protonmail.com>
2019-07-07 23:03:59 -0400

    added a basic win screen, lose screen, and click listeners to trigger them

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,30 @@
+
+
 var wrongCount = 0
 var rightCount = 0
+
+// COUNTDOWN TO 0
 var timer = 60
 
-
+// ARRAY OF USED QUESTIONS TO CHECK AGAINST WHEN PICKING NEW QUESTIONS
 var usedPool = []
 
+var tempCorrect = ''
+
+
+
+// TRIVIA DATA ARRAY
 var triviaPool = [
+
+    //EACH QUESTION IS AN OBJECT CONTAINING
     q1 = {
+        // the actually question
         question: 'Is this a question?',
 
+        // the answer choices to update the DOM with
         answerChoices: ['yes','yes','no','yes technically but...'],
 
+        // the correct answer stored seperately to check input against
         correctAnswer: 'yes technically but...'
     },
 
@@ -21,9 +35,71 @@ var triviaPool = [
 
         correctAnswer: 'I sure hope so',
     },
+
+    // q3 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
+
+    // q4 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
+
+    // q5 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
+
+    // q6 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
+
+    // q7 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
+
+    // q8 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
+
+    // q9 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
+
+    // q10 = {
+    //     question: ,
+
+    //     answerChoices: [],
+
+    //     correctAnswer: ,
+    // },
 ]
-
-
 
 
 
@@ -33,24 +109,59 @@ function randomNum(fromNum,toNum) {
     return number
 }
 
+function youWon(){
+    $('#question').text('CORRECT!!!')
+    for (i=0; i < 4; i++){
+        $('#a' + i).text('')
+        $('#a' + i).attr('data-answer','')
+    }
+}
+
+function youFailed(){
+    $('#question').text('YOU FAILED')
+    for (i=0; i < 4; i++){
+        $('#a' + i).text('')
+        $('#a' + i).attr('data-answer','')
+    }
+}
+
 
 function newQuestion () {
+    // PICKS RANDOM QUESTION
     var base = triviaPool[randomNum(0,2)]
-
-    if (usedPool.includes(base)) {
+    
+    
+    // TEST FOR REPEATING QUESTIONS
+    if (usedPool.includes(base)) {                                  
         newQuestion()
     }
-
+    
+    // UPDATE DOM
     $('#question').text(base.question)
-
+    
     for (i=0; i < base.answerChoices.length; i++){
         $('#a' + i).text(base.answerChoices[i])
         $('#a' + i).attr('data-answer',base.answerChoices[i])
     }
-
+    
+    // ADDS QUESTION TO THE USED ARRAY TO CHECK AGAINST 
     usedPool.push(base)
+    tempCorrect = base.correctAnswer
 }
 
 
+
+$('.answerChoice').on('click', function(){
+    var choice = $(this).attr('data-answer')
+    
+    if (choice === tempCorrect){
+        youWon()
+    } else {
+        youFailed()
+    }
+})
+
+
+// INITIALIZATION-------------------------------------------------------------------------------------------
 
 newQuestion()
