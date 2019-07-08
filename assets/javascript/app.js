fc308935c4a7eabaa1c02275d778279385cc8cc6
Max Szczepaniak <wookslandline@protonmail.com>
2019-07-08 14:28:00 -0400

    added timer element and game over screen.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,11 +4,12 @@ var wrongCount = 0
 var rightCount = 0
 
 // COUNTDOWN TO 0
-var timer = 60
+var countdown = 30
 
 // ARRAY OF USED QUESTIONS TO CHECK AGAINST WHEN PICKING NEW QUESTIONS
 var usedPool = []
 
+var questionCount = 0
 var tempCorrect = ''
 
 
@@ -36,13 +37,13 @@ var triviaPool = [
         correctAnswer: 'I sure hope so',
     },
 
-    // q3 = {
-    //     question: ,
+    q3 = {
+        question:'This is the third question, how are you?' ,
 
-    //     answerChoices: [],
+        answerChoices: ['eh','good','bad','so tired, I am so tired'],
 
-    //     correctAnswer: ,
-    // },
+        correctAnswer: 'so tired, I am so tired',
+    },
 
     // q4 = {
     //     question: ,
@@ -101,7 +102,13 @@ var triviaPool = [
     // },
 ]
 
-
+function clear() {
+    $(".answerChoice").empty()
+    $(".answerChoice").attr('style','display: none')
+    $('#timer').empty()
+    $(".appendage").attr('style','display: none')
+    
+}
 
 
 function randomNum(fromNum,toNum) {
@@ -110,56 +117,88 @@ function randomNum(fromNum,toNum) {
 }
 
 function youWon(){
+    rightCount++
     $('#question').text('CORRECT!!!')
-    for (i=0; i < 4; i++){
-        $('#a' + i).text('')
-        $('#a' + i).attr('data-answer','')
-    }
+    clear()        
+    setTimeout(function(){ newQuestion(); }, 3000);
 }
 
 function youFailed(){
+    wrongCount++
     $('#question').text('YOU FAILED')
-    for (i=0; i < 4; i++){
-        $('#a' + i).text('')
-        $('#a' + i).attr('data-answer','')
-    }
+    clear()
+    setTimeout(function(){ newQuestion(); }, 3000);
+}
+
+function endScreen(){
+    clear()
+    $('#question').text('GAME OVER')
+    $('#answerList').append('<p class="appendage" style="display: block">Correct Answers: ' + rightCount + '</p>')
+    $('#answerList').append('<p class="appendage" style="display: block">Failures: ' + wrongCount + '</p>')
 }
 
 
 function newQuestion () {
-    // PICKS RANDOM QUESTION
-    var base = triviaPool[randomNum(0,2)]
+
+    countdown = 10
     
+    timer = setInterval(function(){
+        countdown--
+        if (countdown < 1) {
+            clearInterval(timer)
+            youFailed()
+         }else{   
+            $('#timer').text(countdown)
+        }
+    },1000);    
     
-    // TEST FOR REPEATING QUESTIONS
-    if (usedPool.includes(base)) {                                  
-        newQuestion()
+    if (questionCount >= triviaPool.length) {
+        clearInterval(timer)
+        endScreen()
+    } else {
+        
+        // PICKS RANDOM QUESTION
+        var base = triviaPool[randomNum(0,triviaPool.length)]
+        
+        
+        // TEST FOR REPEATING QUESTIONS
+        if (usedPool.includes(base)) {                                  
+            base = triviaPool[randomNum(0,triviaPool.length)]
+        }
+        
+        // UPDATE DOM
+        $('#question').text(base.question)
+        
+        for (i=0; i < base.answerChoices.length; i++){
+            $('.answerChoice').attr('style','display: block')
+            $('#a' + i).text(base.answerChoices[i])
+            $('#a' + i).attr('data-answer',base.answerChoices[i])
+        }
+        
+
+        // ADDS QUESTION TO THE USED ARRAY TO CHECK AGAINST 
+        usedPool.push(base)
+        
+        questionCount++
+        tempCorrect = base.correctAnswer
+
+        
     }
-    
-    // UPDATE DOM
-    $('#question').text(base.question)
-    
-    for (i=0; i < base.answerChoices.length; i++){
-        $('#a' + i).text(base.answerChoices[i])
-        $('#a' + i).attr('data-answer',base.answerChoices[i])
-    }
-    
-    // ADDS QUESTION TO THE USED ARRAY TO CHECK AGAINST 
-    usedPool.push(base)
-    tempCorrect = base.correctAnswer
 }
 
 
 
 $('.answerChoice').on('click', function(){
     var choice = $(this).attr('data-answer')
+    clearInterval(timer)
+        if (choice === tempCorrect){
+            youWon()
+        } else {
+            youFailed()
+        }
     
-    if (choice === tempCorrect){
-        youWon()
-    } else {
-        youFailed()
-    }
 })
+
 
 
 // INITIALIZATION-------------------------------------------------------------------------------------------
